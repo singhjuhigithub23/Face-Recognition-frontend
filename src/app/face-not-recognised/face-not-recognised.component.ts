27096bad9298499d99236c3583a38aa34983ef3e
Juhi Singh <juhisingh@astreya.com>
2023-11-07 15:13:13 +0530

    commit

--- src/app/face-not-recognised/face-not-recognised.component.ts
+++ src/app/face-not-recognised/face-not-recognised.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-face-not-recognised',
@@ -7,6 +9,11 @@ import { Component } from '@angular/core';
 })
 export class FaceNotRecognisedComponent {
   img1="../assets/face_not_recognised.png"
+  constructor(private router: Router) {}
+  onsave(){
+
+    this.router.navigate(['/popup']);
+    }
 
 
 }
